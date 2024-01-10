db3f96ce0bf7626a88bdaf53fe4442019db317a2
Kimhout-Theam <155095258+Kimhout-Theam@users.noreply.github.com>
2024-01-10 20:08:44 +0700

    Add files via upload

---
+++ MidtermEx1/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.time.Month;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the short date (YYYY-MM-DD): ");
+        String date = input.nextLine();
+        String[] split = date.split("-");
+      int month = Integer.parseInt(split[1]);
+      Month monthName = Month.of(month);
+      System.out.println(monthName +" " + split[2]+", "+split[0]);
+        }
+    }
