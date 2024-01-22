3335b0ba8d243e0f4fe7366af950337bdb0a7098
Rainz-23 <movin430630@gmail.com>
2024-01-22 18:25:56 +0700

    FirstCommit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Random random = new Random();
+        int wrong = 0;
+        System.out.println("Welcome To Guessing Number Game");
+        System.out.print("Input Max Number: ");
+        int max = input.nextInt();
+        System.out.print("Input Min Number: ");
+        int min = input.nextInt();
+        int randomNumber = random.nextInt(max - min + 1) + min;
+
+        while(true) {
+            System.out.print("Please Input Your Number: ");
+            int user_answer = input.nextInt();
+            if (user_answer == randomNumber) {
+                System.out.println("Bravo Your Guess is correct!!");
+                System.out.println("Correct Answer: " + randomNumber);
+                System.out.println("Wrong Guessed: " + wrong);
+                return;
+            } else if (user_answer > max || user_answer < min) {
+                System.out.println("The Number Entered is Out of Range!!");
+            } else {
+                ++wrong;
+                System.out.println("Incorrect! Please Try Again!");
+            }
+            input.close();
+        }
+    }
+}
